fe514313b93b4707e6efe5c623d76d295e12a106
kayguyx2 <kayguyx2@gmail.com>
2022-01-13 22:56:14 +0700

    Add shared styles

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+type COLOR_TYPE = string;
+
+export const PRIMARY: COLOR_TYPE = '#1779ba';
+export const SECONDARY: COLOR_TYPE = '#767676';
+export const WHITE: COLOR_TYPE = '#FFFFFF';
+export const BLACK: COLOR_TYPE = '#000000';
+
+// ACTIONS
+export const SUCCESS: COLOR_TYPE = '#3adb76';
+export const WARNING: COLOR_TYPE = '#ffae00';
+export const ALERT: COLOR_TYPE = '#cc4b37';
+
+// GRAYSCALE
+export const GRAY_LIGHT: COLOR_TYPE = '#e6e6e6';
+export const GRAY_MEDIUM: COLOR_TYPE = '#cacaca';
+export const GRAY_DARK: COLOR_TYPE = '#8a8a8a';
