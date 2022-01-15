d35a9fe67d99a3d6b82d50a10c6e425f333661e1
kayguyx2 <kayguyx2@gmail.com>
2022-01-15 16:52:24 +0700

    Adding font size 14

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -11,6 +11,7 @@ export const FONT_SIZE_24 = scaleFont(24);
 export const FONT_SIZE_20 = scaleFont(20);
 export const FONT_SIZE_18 = scaleFont(18);
 export const FONT_SIZE_16 = scaleFont(16);
+export const FONT_SIZE_14 = scaleFont(14);
 
 // LINE HEIGHT
 export const LINE_HEIGHT_28 = scaleFont(28);
