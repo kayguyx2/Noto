1b323664e2dbb47689dd91a1a6c0e2c5288c1a07
kayguyx2 <kayguyx2@gmail.com>
2022-01-17 18:37:44 +0700

    move home interface

---
+++ src/screens/Home/home.interface.ts
@@ -0,0 +1,5 @@
+import { INote } from "@/store/types";
+
+export interface HomeScreenProps {
+    listsNote: INote[];
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,14 @@
 import React, {FunctionComponent} from 'react';
 import EmptyList from '@/components/EmptyList';
-import {INote, IStoreState} from '@/store/types';
+import { IStoreState} from '@/store/types';
 import {connect} from 'react-redux';
 import Layout from '@/components/Layout';
-import {Text} from 'react-native';
 import ListsNote from '@/components/ListsNote';
 import HeaderMain from '@/components/HeaderMain';
 import {Colors} from '@/styles';
+import { HomeScreenProps } from './home.interface';
 
-interface HomeScreenProps {
-    listsNote: INote[];
-}
+
 
 const HomeScreen: FunctionComponent<HomeScreenProps> = ({listsNote}) => {
     if (listsNote.length === 0) {
